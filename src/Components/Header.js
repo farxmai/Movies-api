7bc55cfe4f6eb863433d8d8b4408130371f4420c
Farxmai <Farxmai2@gmail.com>
2019-12-09 02:45:51 +0200

    Search has been updated

--- src/Components/Header.js
+++ src/Components/Header.js
@@ -1,13 +1,15 @@
-import React from 'react';
+import React, { useState } from 'react';
 import 'bootstrap/dist/css/bootstrap.min.css';
-import { Navbar, Nav, Form, FormControl } from 'react-bootstrap';
+import { Navbar, Nav, Form, FormControl, Button } from 'react-bootstrap';
 import { NavLink } from "react-router-dom";
 import { setSearchQuery } from '../Store/Actions'
 import { useDispatch } from "react-redux"
 
 function Header() {
     const dispatch = useDispatch()
-    const search = (event) => {dispatch(setSearchQuery(event.target.value))}
+    const [searcInput, setSearcInput] = useState("")
+    const search = (event) => {setSearcInput(event.target.value)}
+    const SearchQuery = () => {dispatch(setSearchQuery(searcInput))}
     return (    
         <Navbar bg="dark" expand="md" variant="dark" fixed="top">
             <Navbar.Brand href="/">Movies</Navbar.Brand>
@@ -24,7 +26,8 @@ function Header() {
                 placeholder="Search" 
                 className="mr-sm-2" 
                 onChange={search}
-                />        
+                />
+            <Button variant="outline-success" onClick={SearchQuery}>Search</Button>        
             </Form>
         </Navbar.Collapse>
         </Navbar>
